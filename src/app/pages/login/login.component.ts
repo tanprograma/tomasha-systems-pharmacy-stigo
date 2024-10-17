1c6f407544c3877849de541461ead40cb1e8cf60
tanprograma <tanprograma@gmail.com>
2024-10-17 13:31:04 +0300

    fixed the stores for otherS

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ShopService } from '../../services/shop.service';
 import { User } from '../../interfaces/user';
+import { Store } from '../../interfaces/store';
 
 @Component({
   selector: 'app-login',
@@ -28,8 +29,31 @@ export class LoginComponent {
       if (res.status) {
         const user = res.result as User;
         this.shopService.setCurrentUser(user);
-        this.shopService.router.navigate(['/home']);
+        this.redirectToStore(user);
       }
+      // this.shopService.router.navigate(['/home']);
+    });
+  }
+  redirectToStore(user: User) {
+    this.shopService.getStores().subscribe((stores) => {
+      const store = this.findStore(user.email, stores);
+
+      if (
+        user.email == 'mainclinic' ||
+        user.email == 'eliezermnada@gmail.com' ||
+        store == undefined
+      ) {
+        this.shopService.router.navigate(['/home']);
+        return;
+      }
+      if (store != undefined) {
+        this.shopService.router.navigate(['/shop', store.name, store._id]);
+      }
+    });
+  }
+  findStore(email: string, stores: Store[]) {
+    return stores.find((store) => {
+      return store.name == email;
     });
   }
 }
