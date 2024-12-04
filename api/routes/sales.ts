5e1b31ad76299fbfb7c63119986db4f3db1a7d88
tanprograma <tanprograma@gmail.com>
2024-12-04 17:32:54 +0300

    fixes on sales route

--- api/routes/sales.ts
+++ api/routes/sales.ts
@@ -1,6 +1,7 @@
 import Express from 'express';
 import { SaleUtil } from '../utils/sale';
 import { SaleModel } from '../models/sale';
+import { Sale } from '../../src/app/interfaces/sale';
 const router = Express.Router();
 router.get('/', async (req, res) => {
   const sales = await SaleUtil.getAllSales();
@@ -22,4 +23,39 @@ router.post('/createmany', async (req, res) => {
   const sales = await SaleUtil.createSales(req.body);
   res.send({ status: true, result: sales });
 });
+router.patch('/date/:priordate/:newdate', async (req, res) => {
+  // changing date for dispensed items
+  const priorDate = req.params.priordate;
+
+  try {
+    const allSales = await SaleModel.find();
+
+    const newDate = new Date(req.params.newdate).toISOString();
+    const sales = allSales.filter((sale: any) => {
+      const date = getDateString(sale.createdAt as string);
+
+      return date == priorDate;
+    });
+
+    console.log({ sales: sales.length });
+    for (let item of sales) {
+      const s = await SaleModel.findOneAndDelete({ _id: item._id });
+
+      await SaleModel.create({
+        store: item.store,
+        products: item.products,
+        discount: item.discount || 0,
+        customer: item.customer,
+        createdAt: newDate,
+      });
+    }
+    res.send({ status: true });
+  } catch (error) {
+    res.send({ status: false });
+  }
+});
+function getDateString(d: string) {
+  const date = new Date(d);
+  return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+}
 export default router;
